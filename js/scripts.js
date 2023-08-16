14cb8221ab74f924ccbd1406a9e5a373ea4e66e6
Jonathan Cheng <jcheng461@gmail.com>
2023-08-16 14:01:16 -0700

    refactor for loop to be simpler. WIP: have function handle edge cases
    co-authored-by: Ian Bravo <bravo.ian@gmail.com>
    Co-authored-by: Jonathan Lu <jdlu.coding@gmail.com>
    Co-authored-by: Paul LeTourneau <pletourneau62@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,17 @@
 // Business Logic
 function countUpBy(endNumber, multiplier) {
-  let finalSum = multiplier;
-  for (let i = multiplier; i < endNumber; i += multiplier) {
-    finalSum += multiplier;
+  if (endNumber.trim() === 0 || multiplier.trim() === 0) {
+    return null;
+  } else if () {
+    
   }
-  return finalSum;
+
+  const finalArray = [];
+
+  for (let i = multiplier; i <= endNumber; i += multiplier) {
+    finalArray.push(i);
+  }
+  return finalArray;
 }
 
 // User Interface Logic
