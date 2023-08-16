a55d5716d10862d648d716ce3ae673c85b853060
Jonathan Cheng <jcheng461@gmail.com>
2023-08-16 13:18:47 -0700

    pass 4th test for countUpBy() function
    co-authored-by: Ian Bravo <bravo.ian@gmail.com>
    Co-authored-by: Jonathan Lu <jdlu.coding@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
 // Business Logic
 function countUpBy(endNumber, multiplier) {
-  return (multiplier += multiplier);
+  let finalSum = multiplier;
+  for (let i = multiplier; i < endNumber; i += multiplier) {
+    finalSum += multiplier;
+  }
+  return finalSum;
 }
 
 // User Interface Logic
